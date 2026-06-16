1c40b7483557697a3a8de2e44a08742177a8e800
NWarila <33955773+NWarila@users.noreply.github.com>
2026-06-16 20:16:03 +0000

    Replace StoreFactory seam with New-X509Store

--- tests/Private/Get-StoreCertificate.Tests.ps1
+++ tests/Private/Get-StoreCertificate.Tests.ps1
@@ -13,28 +13,53 @@ Describe 'Get-StoreCertificate' {
     Mock -CommandName Test-CertificateStoreExporterWindows -MockWith {
       $False
     }
+    Mock -CommandName New-X509Store -MockWith {
+      throw 'Synthetic unexpected store access.'
+    }
 
     {
       Get-StoreCertificate -StoreLocation LocalMachine -StoreName Root
     } | Should -Throw -ErrorId 'NotWindows,New-ErrorRecord'
 
     Should -Invoke -CommandName Test-CertificateStoreExporterWindows -Times 1 -Exactly
+    Should -Invoke -CommandName New-X509Store -Times 0 -Exactly
   }
 
-  It 'wraps store factory failures with StoreReadFailure' {
+  It 'wraps store constructor failures with StoreReadFailure' {
     Mock -CommandName Test-CertificateStoreExporterWindows -MockWith {
       $True
     }
-
-    $StoreFactory = {
-      throw 'Synthetic store-open failure.'
+    Mock -CommandName New-X509Store -MockWith {
+      throw 'Access is denied.'
     }
 
     {
       Get-StoreCertificate `
         -StoreLocation LocalMachine `
-        -StoreName Root `
-        -StoreFactory $StoreFactory
+        -StoreName Root
     } | Should -Throw -ErrorId 'StoreReadFailure,New-ErrorRecord'
+
+    Should -Invoke -CommandName New-X509Store -Times 1 -Exactly -ParameterFilter {
+      ($Name -eq 'Root') -and
+      ($Location -eq [System.Security.Cryptography.X509Certificates.StoreLocation]::LocalMachine)
+    }
+  }
+}
+
+Describe 'New-X509Store' {
+  BeforeAll {
+    . (Join-Path -Path $PSScriptRoot -ChildPath '..\..\build\Export-CertificateStoreBundle.Functions.ps1')
+  }
+
+  It 'creates an X509Store for the requested name and location' {
+    $Store = New-X509Store -Name Root -Location LocalMachine
+
+    try {
+      $Store | Should -BeOfType ([System.Security.Cryptography.X509Certificates.X509Store])
+      $Store.Name | Should -Be 'Root'
+      $Store.Location | Should -Be ([System.Security.Cryptography.X509Certificates.StoreLocation]::LocalMachine)
+    } finally {
+      $Store.Dispose()
+    }
   }
 }
